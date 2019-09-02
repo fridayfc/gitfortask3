c1289e0c2d46daf349b930beb2ad22e69f2efa96
Type-WYB <49379064+Type-WYB@users.noreply.github.com>
2019-09-02 13:07:35 +0800

    Commit

--- src/decorator/Client.java
+++ src/decorator/Client.java
@@ -1,6 +1,8 @@
 //designpatterns.decorator.Client.java
 package decorator;
 
+import com.sun.org.glassfish.gmbal.Description;
+
 public class Client {
 	public static void main(String args[]) {
 		Component component, componentSB, componentBB;  //使用抽象构件定义全部对象
@@ -8,5 +10,24 @@ public class Client {
 		componentSB = new ScrollBarDecorator(component); //创建装饰后的构件对象        
 		componentBB = new BlackBorderDecorator(componentSB); //将装饰了一次的对象注入另一个装饰类中，进行第二次装饰
 		componentBB.display();
+
+		/**
+		*@Author 201717025246王益波
+		*@Date 2019/9/2
+		*@Time 12:45
+		*/
+		Component component1;//使用抽象构件定义全部对象
+		component1 = new ListBox();         //创建具体构件对象
+		component1 = new ScrollBarDecorator(component1); //创建装饰后的构件对象
+		component1 = new BlackBorderDecorator(component1); //将装饰了一次的对象注入另一个装饰类中，进行第二次装饰
+		component1.display();
+       System.out.println();
+		Component component2;//使用抽象构件定义全部对象
+		component2 = new MyFrame();         //创建具体构件对象
+		component2 = new ScrollBarDecorator(component2); //创建装饰后的构件对象
+		component2 = new BlackBorderDecorator(component2); //将装饰了一次的对象注入另一个装饰类中，进行第二次装饰
+		component2 =new MyDecorator(component2);
+		component2.display();
+
 	}
 }
---
+++ src/decorator/MyDecorator.java
@@ -0,0 +1,18 @@
+package decorator;
+
+/**
+ * @author 201717025246王益波
+ * @version 1.0
+ * @date 2019/9/2 13:01
+ */
+public class MyDecorator extends ComponentDecorator {
+    public MyDecorator(Component component) {
+        super(component);
+    }
+
+    @Override
+    public void display() {
+        super.display();//显示父类即组件的display方法
+        System.out.println("显示MyDecorator");
+    }
+}
